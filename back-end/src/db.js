e641b98ccde96eb65f156f38c8ae124515019cd2
Mohammad Arif <arif_iq@yahoo.co.in>
2022-05-08 15:09:42 +0530

    Sendgrid email setup.

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb'
 let client
 
 export const initializeDbConnection = async () => {
-  client = await MongoClient.connect('mongodb://localhost:27017', {
+  client = await MongoClient.connect('mongodb://127.0.0.1:27017', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,6 +1,13 @@
 import { testRoute } from './testRoute'
+// import { testEmailRoute } from './testEmailRoute'
 import { signUpRoute } from './signUpRoute'
 import { loginRoute } from './loginRoute'
 import { updateUserInfoRoute } from './updateUserInfoRoute'
 
-export const routes = [updateUserInfoRoute, loginRoute, signUpRoute, testRoute]
+export const routes = [
+  updateUserInfoRoute,
+  // testEmailRoute,
+  loginRoute,
+  signUpRoute,
+  testRoute
+]
