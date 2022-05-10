2aa4b708c7b55b11e5b3c7f4d567ed51fdd39afa
Mohammad Arif <arif_iq@yahoo.co.in>
2022-05-10 09:54:17 +0530

    Fix the id is not defined issue while updating user info.

--- back-end/src/routes/updateUserInfoRoute.js
+++ back-end/src/routes/updateUserInfoRoute.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
-import { ObjectID } from 'mongodb'
+import { ObjectId } from 'mongodb'
 import { getDbConnection } from '../db'
 
 export const updateUserInfoRoute = {
@@ -9,24 +9,18 @@ export const updateUserInfoRoute = {
     const { authorization } = req.headers
     const { userId } = req.params
 
-    console.log('userId', userId, ' authorization', authorization)
     // Check and see the authorization header is included
     if (!authorization) {
       return res.status(401).json({ message: 'No authorization header sent' })
     }
 
-    //
     const updates = (({ favoriteFood, hairColor, bio }) => ({
       favoriteFood,
       hairColor,
       bio
     }))(req.body)
 
-    console.log('updates', updates)
-
-    // Bearer token
     const token = authorization.split(' ')[1]
-    console.log('token', token)
 
     /**
      * jwt.verify(token, secretOrPublicKey, [options, callback])
@@ -36,11 +30,7 @@ export const updateUserInfoRoute = {
       if (err)
         return res.status(401).json({ message: 'Unable to verify token' })
 
-      console.log('decoded', decoded)
-
-      // If the token is valid, get the id from the token
       const { id, isVerified } = decoded
-      console.log('decoded id', id, ' userId', userId)
 
       // In case id don't match with the userId from the request param, return error to the client
       if (id !== userId)
@@ -52,41 +42,37 @@ export const updateUserInfoRoute = {
         return res.status(403).json({
           message: 'Please verify your email before you update the data.'
         })
-    })
 
-    // If the id and userId from the request param match, update the user info
-    const db = getDbConnection('react-auth-db')
-    const result = await db
-      .collection('users')
+      // Get a connection to the database
+      const db = getDbConnection('react-auth-db')
+      const result = await db
+        .collection('users')
+        .findOneAndUpdate(
+          { _id: ObjectId(userId) },
+          { $set: { info: updates } },
+          { returnOriginal: false }
+        )
+
+      // If the user info is updated, return the updated user info to the client
+      const { email, info } = result.value
+
       /**
-       * db.collection.findOneAndUpdate( filter, update, options )
+       * jwt.sign(payload, secretOrPrivateKey, [options, callback])
        */
-      .findOneAndUpdate(
-        { _id: ObjectID(userId) },
-        { $set: { info: updates } },
-        { returnOriginal: false }
+      jwt.sign(
+        { id, email, isVerified, info },
+        process.env.JWT_SECRET,
+        { expiresIn: '2d' },
+        (err, token) => {
+          // If there was an error, return an error to the client
+          if (err) {
+            return res.sendStatus(500)
+          }
+
+          // Otherwise, return the token to the client
+          res.status(200).json({ token })
+        }
       )
-
-    console.log('result', result)
-
-    // If the user info is updated, return the updated user info to the client
-    const { email, info } = result.value
-
-    // console.log('Before jwt.sign', email, isVerified, info)
-    /**
-     * jwt.sign(payload, secretOrPrivateKey, [options, callback])
-     */
-    jwt.sign(
-      { id, email, isVerified, info },
-      process.env.JWT_SECRET,
-      { expiresIn: '2d' },
-      (err, token) => {
-        // If there is an error, return error to the client
-        if (err) return res.status(200).json(err)
-
-        // Otherwise return the token to the client
-        res.status(200).json({ token })
-      }
-    )
+    })
   }
 }
